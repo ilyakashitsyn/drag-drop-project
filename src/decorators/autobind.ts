1d348b4959b6d80f5a83bf7384876449513b092a
Ilya Kashitsyn <ilyakashitsyn@gmail.com>
2025-07-05 04:50:21 -0300

    refactoring: using ES modules

--- src/decorators/autobind.ts
+++ src/decorators/autobind.ts
@@ -1,18 +1,16 @@
-namespace App {
-  // Autobind decorator
-  export function autobind(
-    _target: any,
-    _methodName: string,
-    description: PropertyDescriptor,
-  ) {
-    const originalMethod = description.value;
-    const adjDescriptor: PropertyDescriptor = {
-      configurable: true,
-      get() {
-        const boundFn = originalMethod.bind(this);
-        return boundFn;
-      },
-    };
-    return adjDescriptor;
-  }
+// Autobind decorator
+export function autobind(
+  _target: any,
+  _methodName: string,
+  description: PropertyDescriptor,
+) {
+  const originalMethod = description.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
 }
